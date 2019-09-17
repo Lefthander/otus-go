ffde9a8a7e89d1d8c67bcfc6b600d1c5a0038dc6
Sergey Olisov <sergey.olisov@ericsson.com>
2019-09-17 15:13:40 +0300

    Fixed issue with upredictable output

--- hm-3/textfrq.go
+++ hm-3/textfrq.go
@@ -27,7 +27,8 @@ func (w Words) Len() int {
 
 // Less compare two elements of array words by frequency.
 func (w Words) Less(i, j int) bool {
-	return w[i].frequency < w[j].frequency
+	// Additional check is added to avoid unpredictable result of Sort function when the frequency of word are the same.
+	return w[i].frequency < w[j].frequency || (w[i].frequency == w[j].frequency && w[i].word > w[j].word)
 }
 
 // Swap two elements of array words
@@ -40,8 +41,8 @@ func (w Words) Swap(i, j int) {
 // warning has returned. I.e. Error - when the input text is empty. Waring in case of lenght of imput
 // text less then 10 words.
 func TextFrq(s string) Words {
-	re := regexp.MustCompile(`\w{2,}`)
-	text := re.FindAllString(strings.ToLower(s), -1)
+	re := regexp.MustCompile(`\w{2,}`)               // Considering the word is greater then one character. Can be tuned.
+	text := re.FindAllString(strings.ToLower(s), -1) // All input passed through ToLower in order to filter out such cases as Word != word
 	mapWords := map[string]int{}
 	for _, w := range text {
 		mapWords[w]++
@@ -60,35 +61,5 @@ func TextFrq(s string) Words {
 }
 func main() {
 	testText := "aaaa bbb aaaa bbb cccc ddddd aaaa xxxx zzzzz fffff rrrrr eeee aaaa bbb rrrrr"
-	sampletest2 := `Bacon ipsum dolor amet pork chop sunt swine, t-bone velit pastrami frankfurter adipisicing.
-					Elit sunt sirloin sed eu. Eu qui swine cillum filet mignon ut prosciutto dolore.
-					Deserunt reprehenderit pork, rump flank consequat magna consectetur. Mollit shoulder salami
-					reprehenderit. Boudin flank turkey biltong beef porchetta. Brisket t-bone eiusmod beef ribs
-					pig dolor ullamco spare ribs tail non strip steak shoulder sunt.
-					Leberkas venison culpa prosciutto beef reprehenderit dolore strip steak. Eu pork chop irure
-					drumstick in dolore. Commodo flank boudin, incididunt t-bone lorem sed strip steak eu.
-					Tri-tip do et voluptate. In magna exercitation beef turkey quis, ball tip pancetta laborum
-					nisi id. Meatloaf boudin porchetta sirloin brisket biltong aute adipisicing non turkey chuck tempor.
-					Tongue dolore ipsum occaecat jerky fatback salami shankle. Et in meatloaf excepteur tempor pork.
-					In jowl landjaeger short loin sed ut tail eiusmod elit strip steak ut ribeye duis ut. Short ribs
-					beef ribs laboris sed tongue ea pig prosciutto lorem salami bacon est. Ut elit ipsum, dolor ea
-					t-bone voluptate jowl chicken commodo jerky ex picanha esse. Dolore bacon andouille non irure
-					tail pariatur.`
-	sampletest3 := `Video provides a powerful way to help you prove your point. When you click Online Video, you can
-				  paste in the embed code for the video you want to add. You can also type a keyword to search online
-				  for the video that best fits your document. To make your document look professionally produced,
-				  Word provides header, footer, cover page, and text box designs that complement each other. For 
-				  example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the
-				  elements you want from the different galleries. Themes and styles also help keep your document
-				  coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt
-				  graphics change to match your new theme. When you apply styles, your headings change to match the
-				  new theme. Save time in Word with new buttons that show up where you need them.`
-	// Regexp to extract words from the text, all punctuations and spaces will be removed.
-	//re := regexp.MustCompile("\\w+")
-	//text := re.FindAllString(sampletest2, -1)
-
-	fmt.Println(TextFrq(sampletest2))
 	fmt.Println(TextFrq(testText))
-	fmt.Println(TextFrq(sampletest3))
-	//fmt.Println(textfrq(sampletest2))
 }

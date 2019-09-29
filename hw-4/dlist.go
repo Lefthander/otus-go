5ca7bc99ae0b53292a62f231c3f233d002714679
Sergey Olisov <sergey.olisov@ericsson.com>
2019-09-29 18:49:40 +0300

    Working with tests

--- hw-4/dlist.go
+++ hw-4/dlist.go
@@ -1,6 +1,6 @@
 // Sergey Olisov 2019 (c)
 // Lesson 4
-// Yet another one implementation of Double Linked list
+// Yet another implementation of Double Linked list :)
 
 package main
 
@@ -14,73 +14,144 @@ First() // первый Item
 Last() // последний Item
 PushFront(v interface{}) // добавить значение в начало
 PushBack(v interface{}) // добавить значение в конец
-Remove(i Item) // удалить элемент​
-
+Remove(i Item) // удалить элемент​ Hmmm logically remove should work with v:interface{}
+ As we made add new item by value...
 Item // элемент списка
 Value() interface{} // возвращает значение
 Nex() *Item // следующий Item
 Prev() *Item // предыдущий
 */
 
+// Item element of Double Linked List
 type Item struct {
-	DataContainer interface{}
-	dl            *DList // Doual Linked list which this element belongs to
-	nextItem      *Item  // Pointer to next Item
-	prevItem      *Item  // Pointer to previous Item
+	DataContainer interface{} // Container to store the data
+	nextItem      *Item       // Pointer to next Item
+	prevItem      *Item       // Pointer to previous Item
 }
 
+// DList structure to store the components of the double linked list
 type DList struct {
 	Head   *Item // Head of the list
 	Tail   *Item // Tail of the list
 	Length int   // Lenght of the list
 }
 
-func (d *DList) Initialize() *DList {
-	d.Head.nextItem = d.Tail
-	d.Head.prevItem = nil
-	d.Tail.nextItem = nil
-	d.Tail.prevItem = d.Head
-	d.Length = 0
-	return d
-}
-func createDList() *DList {
-	return new(DList).Initialize()
-}
-
+// Len returns lenght of the List
 func (d *DList) Len() int {
 	return d.Length
 }
 
+// First returns the first Item from the List
 func (d *DList) First() *Item {
 	return d.Head
 }
 
+// Last returns the last Item from the List
 func (d *DList) Last() *Item {
 	return d.Tail
 }
 
+// PushFront adds a new item at the begining of the List
 func (d *DList) PushFront(v interface{}) {
 
+	newItem := &Item{DataContainer: v}
+	if d.Length == 0 {
+		d.Head = newItem
+		d.Tail = newItem
+	} else {
+		d.Head.prevItem = newItem
+		newItem.nextItem = d.Head
+		d.Head = newItem
+	}
+	d.Length++
 }
+
+//PushBack add a new Item to the end of the List
 func (d *DList) PushBack(v interface{}) {
-
+	newItem := &Item{DataContainer: v}
+	// Check do we have an empty list... If so new item become a Head
+	if d.Length == 0 {
+		d.Head = newItem
+		d.Tail = newItem
+	} else {
+		newItem.prevItem = d.Tail
+		d.Tail.nextItem = newItem
+		d.Tail = newItem
+	}
+	d.Length++
 }
 
-func (d *DList) Remove(i *Item) {
-
+// Remove deletes the specific element from the List by the element's value.
+func (d *DList) Remove(i *Item) bool {
+	// Check is the List is empty
+	if d.Length == 0 {
+		return false
+	}
+	current := d.Head
+	// Has a match
+	if current == i {
+		current = current.nextItem
+		d.Length--
+		if current != nil {
+			d.Head = current
+			d.Head.prevItem = nil
+		} else {
+			d.Tail = nil
+			d.Head = nil
+		}
+		return true
+	}
+	for current.nextItem != nil {
+		if current.nextItem == i {
+			current.nextItem = current.nextItem.nextItem
+			if current.nextItem == nil { //In case of last Item
+				d.Tail = current
+			} else {
+				current.nextItem.prevItem = current
+			}
+			d.Length--
+			return true
+		}
+		current = current.nextItem
+	}
+	return false
 }
 
+// Print does printing of all elements in the list,
+// Misceleanous function does not requiered in the HW.
+func (d *DList) Print() {
+	i := d.Head
+	for i != nil {
+		fmt.Println(">", i.DataContainer)
+		i = i.nextItem
+	}
+}
+
+// Value returns the value of DataContainer from the specific Item
 func (i *Item) Value() interface{} {
 	return i.DataContainer
 }
+
+// Next returns the next Item
 func (i *Item) Next() *Item {
 	return i.nextItem
 }
 
+// Prev returns previous Item
 func (i *Item) Prev() *Item {
 	return i.prevItem
 }
 
 func main() {
-	fmt.Println("Linked List")
+	mylist := new(DList)
+	mylist.PushFront("one")
+	mylist.PushFront("two")
+	mylist.Print()
+	fmt.Println(mylist.Len())
+	mylist.Remove(mylist.Last())
+	mylist.Print()
+	fmt.Println(mylist.Len())
+	mylist.Remove(mylist.First())
+	fmt.Println(mylist.Len())
+
 }
--- hw-4/dlist_test.go
+++ hw-4/dlist_test.go
@@ -1,1 +1,112 @@
 package main
+// Sergey Olisov (c) 2019
+// Lesson 4 Double Linked List tests
+
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+/* The Following test cases will be covered:
+- Create empty list - ensure that length is zero
+- PushFront to empty list 
+- PushFront to non empty list
+- PushBack to empty list
+- PushBack to non empty list
+- PushFront & PushBack ??? (Looks like a dublicated test)
+- Remove from the list of three elements an unknown element.
+- Remove from the list of three elements an element in the middle.
+- Remove from the list the last element.
+- Remove from the list the first element.
+- Remove form the empty list the last element.
+- Check Item method - Prev()
+- Check Item Method - Next()
+- Check Item method - Value()
+*/
+
+func TestEmptyNewList(t *testing.T) {
+	dl:=DList{}
+	require.Equal(t,0,dl.Len(),"New empty List should has a length 0")
+}
+
+func TestPushFront(t *testing.T){
+	dl:=DList{}
+	dl.PushFront("front1") // Verify addition to front of empty list
+	require.Equal(t,1,dl.Len(),"Lenght of list should be 1")
+	require.Equal(t,"front1",dl.First().Value(),"The first value should be - front1")
+
+	dl.PushFront("front2") // Verify addition to front of non empty list.
+	
+	require.Equal(t,2,dl.Len(),"Lenght of list should be 2")
+	require.Equal(t,"front2",dl.First().Value(),"The first value should be - front2")
+}
+
+func TestPushBack(t *testing.T){
+	dl:=DList{}
+	dl.PushBack("back1") // verify addition to the end of empty list
+	require.Equal(t,1,dl.Len(),"The Length of list should be 1")
+	require.Equal(t,"back1",dl.Last().Value(),"The last value should be - back1")
+
+	dl.PushBack("back2") // verify addition to the end of non empty list
+	require.Equal(t,2,dl.Len(),"Lenght of list should be 2")
+	require.Equal(t,"back2",dl.Last().Value(),"The latest value in the list should be - back2")
+}
+func TestCombinedPush(t *testing.T){
+	dl:=DList{}
+	dl.PushFront("front1") //Add from the beginin and from the end 
+	dl.PushBack("back1")
+	require.Equal(t,2,dl.Len(),"Lenght of List should be 2")
+	require.Equal(t,"front1",dl.First().Value(),"Value of the fist element should be - front1")
+	require.Equal(t,"back1",dl.Last().Value(),"Value of the last element should be - back1")
+}
+// Test Item methods ( Prev,Next,Value)
+func TestItemBasicTest(t *testing.T){
+	dl:=DList{}
+	dl.PushFront("One")
+	dl.PushFront("Two")
+	require.Equal(t,dl.Last(),dl.First().Next(),"Check Item.Next(),should  point to the last item")
+	require.Equal(t,dl.First(),dl.Last().Prev(),"Check that Item.Prev() should point to first item")
+	require.Equal(t,"One",dl.Last().Value(),"Check the Item.Value() returns the correct value")
+	require.Equal(t,"Two",dl.First().Value(),"Check the Item.Value() returns the correct value")
+}
+func TestRemove(t *testing.T){
+	dl:=DList{}
+	dl.PushFront("one")
+	dl.PushFront("two")
+	dl.PushFront("three")
+	// Now we have a list like {three}-{two}-{one}
+	// Let's try to remove the unknown element from the list
+	unknownItem:=Item{DataContainer:"four"}
+	require.Equal(t,false,dl.Remove(&unknownItem),"Removal of an unknown element shoud return false")
+	require.Equal(t,3,dl.Len(),"Lenght of List should be 3")
+	require.Equal(t,"one",dl.Last().Value(),"The last item must be one")
+	require.Equal(t,"two",dl.First().nextItem.Value(),"The middle item must be two")
+	require.Equal(t,"three",dl.First().Value(),"The first item must be three")
+	// Let's try to remove the element in the middle of list
+	//result:=dl.Remove(dl.First().nextItem)
+	
+	require.Equal(t,true,dl.Remove(dl.First().nextItem),"The removal should be true")
+	require.Equal(t,2,dl.Len(),"Lenght of List should be 2")
+	require.Equal(t,"one",dl.Last().Value())
+	require.Equal(t,"three",dl.First().Value())
+
+	// So we have {three}-{one}
+	// Let's try to delete the last one
+
+	require.Equal(t,true,dl.Remove(dl.Last()),"The removal should be true")
+	require.Equal(t,1,dl.Len(),"Lenght of List should be 1")
+	require.Equal(t,dl.First().Value(),dl.Last().Value())
+	require.Equal(t,"three",dl.First().Value())
+
+	// And we have -{three}- 
+	// Let's del'em all
+
+	require.Equal(t,true,dl.Remove(dl.First()),"Revomal should be ok - true")
+	require.Equal(t,0,dl.Len(),"Lenght of List should be 0")
+	require.Equal(t,(*Item)(nil),dl.Head,"First should be <nil>")
+	require.Equal(t,(*Item)(nil),dl.Tail,"Last should be <nil>")
+
+	// Just to try to remove something from the empty list
+	require.Equal(t,false,dl.Remove(dl.Last()),"Removal from the empty list should return - false")
+}
